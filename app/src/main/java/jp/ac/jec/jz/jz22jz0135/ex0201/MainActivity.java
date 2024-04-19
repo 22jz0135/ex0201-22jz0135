e9f46d55876b162413b395aac0cb0e8236b98e22
unknown <3jz@R841PC45.jz.jec.ac.jp>
2024-04-19 12:11:48 +0900

    完成

---
+++ app/src/main/java/jp/ac/jec/jz/jz22jz0135/ex0201/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.jec.jz.jz22jz0135.ex0201;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
